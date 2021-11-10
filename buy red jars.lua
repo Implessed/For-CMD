f05f28a74be157d6b0cc9f952ada89413555a05a
Implessed <94027470+Implessed@users.noreply.github.com>
2021-11-10 16:09:59 -0500

    Create buy red jars.lua

---
+++ buy red jars.lua
@@ -0,0 +1,78 @@
+_G.Jars = true
+while _G.Jars and game.PlaceId == 536102540 do
+    wait(0.4)
+    local A_1 = game:GetService("Workspace").FriendlyNPCs["Korin BEANS"]
+    local Event = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatStart
+    Event:FireServer(A_1)
+    wait(0.6)
+    local A_1 = {
+        [1] = "k"
+    }
+    local Event = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+    Event:FireServer(A_1)
+    
+    wait(0.6)
+    local A_1 = {
+        [1] = "k"
+    }
+    local Event = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+    Event:FireServer(A_1)
+    wait(0.6)
+    
+    local A_1 = {
+        [1] = "Jars"
+    }
+    local Event = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+    Event:FireServer(A_1)
+    wait(0.6)
+    
+    local A_1 = {
+        [1] = "k"
+    }
+    local Event = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+    Event:FireServer(A_1)
+    wait(0.6)
+    
+    local A_1 = {
+        [1] = "80"
+    }
+    local Event = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+    Event:FireServer(A_1)
+    wait(0.6)
+    
+    local A_1 = {
+        [1] = "k"
+    }
+    local Event = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+    Event:FireServer(A_1)
+    wait(0.6)
+    
+    local A_1 = {
+        [1] = "Red"
+    }
+    local Event = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+    Event:FireServer(A_1)
+    wait(0.6)
+    
+    local A_1 = {
+        [1] = "k"
+    }
+    local Event = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+    Event:FireServer(A_1)
+    wait(0.6)
+    
+    local A_1 = {
+        [1] = "Yes"
+    }
+    local Event = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+    Event:FireServer(A_1)
+    wait(0.6)
+    
+    local A_1 = {
+        [1] = "k"
+    }
+    local Event = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+    Event:FireServer(A_1)
+    wait(0.6)
+    wait()
+end
